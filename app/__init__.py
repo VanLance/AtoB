a520bd4ca68a24abf02286b28fe576314a1eff76
Dylan <dylan23smith1990@gmail.com>
2022-12-09 09:43:28 -0600

    create application factory

--- app/__init__.py
+++ app/__init__.py
@@ -4,24 +4,27 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-login = LoginManager()
-db = SQLAlchemy()
-migrate = Migrate(compare_type=True)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    login = LoginManager()
+    db = SQLAlchemy()
+    migrate = Migrate(compare_type=True)
 
-db.init_app(app)
-migrate.init_app(app,db)
-login.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app,db)
+    login.init_app(app)
 
-login.login_view = 'auth.login'
-login.login_message = 'Please make sure you are logged in!'
-login.login_message_category = 'warning'
+    login.login_view = 'auth.login'
+    login.login_message = 'Please make sure you are logged in!'
+    login.login_message_category = 'warning'
 
-from app.blueprints.auth.routes import auth as auth_bp
-from app.blueprints.service.routes import service_bp
-from app.blueprints.main import main as main_bp
+    from app.blueprints.auth.routes import auth as auth_bp
+    from app.blueprints.service.routes import service_bp
+    from app.blueprints.main import main as main_bp
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(service_bp)
-app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(service_bp)
+    app.register_blueprint(main_bp)
+
+    return app
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn "app:app"
+gunicorn "app:create_app()"
